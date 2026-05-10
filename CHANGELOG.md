15ba66cca0067c0115196d70410b2f21d6e2eb20
토마스 <123928686+ki960213@users.noreply.github.com>
2026-05-10 19:07:36 +0900

    init

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+<!-- Keep a Changelog guide -> https://keepachangelog.com -->
+
+# Riverpod-graph Changelog
+
+## [Unreleased]
---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+import org.jetbrains.intellij.platform.gradle.TestFrameworkType
+
+plugins {
+    id("org.jetbrains.kotlin.jvm")
+    id("org.jetbrains.kotlin.plugin.compose")
+    id("org.jetbrains.changelog")
+    id("org.jetbrains.intellij.platform")
+}
+
+// Read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin.html
+dependencies {
+    testImplementation(libs.junit)
+
+    // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
+    intellijPlatform {
+        intellijIdea("2025.3.4.1")
+        testFramework(TestFrameworkType.Platform)
+
+        // Add plugin dependencies for compilation here:
+        composeUI()
+    }
+}
